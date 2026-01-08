f3a8314eb25f095f839da13f5d9423cec3e12a2f
yurkovgo <yurkovgo@gmail.com>
2026-01-09 01:47:49 +0300

    UPD on queries.py: added functions to add trainings, machines and settings to db

--- queries.py
+++ queries.py
@@ -9,3 +9,30 @@ def add_user(chat_id):
        inserting = insert(Users).values(chat_id=chat_id)
        session.execute(inserting)
        session.commit()
+
+def print_user_id(chat_id):
+    selecting = select(Users.id).where(Users.chat_id == chat_id)
+    with Session(engine) as session:
+        result = session.execute(selecting).first()[0]
+        print("\n\n\n\n\n", result, "\n\n\n\n\n")
+        session.commit()
+
+def add_training(title, chat_id):
+    selecting = select(Users.id).where(Users.chat_id == chat_id)
+    with Session(engine) as session:
+        user_id = session.execute(selecting).first()[0]
+        inserting = insert(Trainings).values(title=title, user_id=user_id)
+        session.execute(inserting)
+        session.commit()
+def add_machine(title, training_id, weight):
+    with Session(engine) as session:
+        inserting = insert(Machines).values(title=title, training_id=training_id, weight=weight)
+        session.execute(inserting)
+        session.commit()
+def add_setting(title, machine_id, value):
+    with Session(engine) as session:
+        inserting = insert(Settings).values(title=title, machine_id=machine_id, value=value)
+        session.execute(inserting)
+        session.commit()
+
+
